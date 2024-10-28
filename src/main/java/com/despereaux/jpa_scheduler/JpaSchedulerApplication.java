6b64b448f5c53e8f2376a711882447099dffdba6
Despereaux <rlaehdwn025@naver.com>
2024-10-28 18:08:02 +0900

    Initial commit

---
+++ src/main/java/com/despereaux/jpa_scheduler/JpaSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.despereaux.jpa_scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaSchedulerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaSchedulerApplication.class, args);
+    }
+
+}
