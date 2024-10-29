6667b5fa3b789bda12428b29c07060f6fc33fc02
Despereaux <rlaehdwn025@naver.com>
2024-10-29 12:25:02 +0900

    feat : 댓글 CRUD 구현, 일정 & 댓글 연관관계 구현, 일정을 삭제할 때 댓글도 삭제되는 기능 구현 fix : 일정 Entity id long -> Long 으로 변경
    일정 페이징 조회 기능 구현 예정

---
+++ src/main/java/com/despereaux/jpa_scheduler/controller/CommentController.java
@@ -0,0 +1,45 @@
+package com.despereaux.jpa_scheduler.controller;
+
+import com.despereaux.jpa_scheduler.dto.CommentRequestDto;
+import com.despereaux.jpa_scheduler.dto.CommentResponseDto;
+import com.despereaux.jpa_scheduler.service.CommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/schedules/{scheduleId}/comments")
+@RequiredArgsConstructor
+public class CommentController {
+
+    private final CommentService commentService;
+
+    @PostMapping("/")
+    public void createComment(@PathVariable Long scheduleId, @RequestBody CommentRequestDto requestDto) {
+        commentService.createComment(scheduleId, requestDto);
+    }
+
+    @GetMapping("/")
+    public List<CommentResponseDto> getAllComments(@PathVariable Long scheduleId) {
+        return commentService.getAllComments(scheduleId);
+    }
+
+    @GetMapping("/{commentId}")
+    public CommentResponseDto getCommentById(@PathVariable Long scheduleId, @PathVariable Long commentId) {
+        return commentService.getCommentById(scheduleId, commentId);
+    }
+
+    @PutMapping("/{commentId}")
+    public void updateComment(
+            @PathVariable Long scheduleId,
+            @PathVariable Long commentId,
+            @RequestBody CommentRequestDto requestDto) {
+        commentService.updateComment(scheduleId, commentId, requestDto);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE) // @DeleteMapping 애너테이션 사용 불가
+    public void deleteComment(@PathVariable Long scheduleId, @PathVariable Long commentId) {
+        commentService.deleteComment(scheduleId, commentId);
+    }
+}
